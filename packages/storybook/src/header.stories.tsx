5e1f72ce86ec370c20d900f4fe762398560cdc0f
dlarrota <dlarrota@hugeinc.com>
2020-08-10 18:24:44 -0500

    feature/storybook Consuming UI from library

---
+++ packages/storybook/src/header.stories.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Header } from "@elmono/ui";
+
+export default {
+  title: "TestComponent"
+};
+
+export const Primary = () => <Header theme="primary" />;
+
+export const Secondary = () => <Header theme="secondary" />;
--- packages/ui/src/components/header/index.tsx
+++ packages/ui/src/components/header/index.tsx
@@ -4,14 +4,14 @@ import HeaderProps from "./header.types";
 
 import "./header.scss";
 
-const TestComponent: React.FC<HeaderProps> = ({ theme }) => (
+const Header: React.FC<HeaderProps> = ({ theme }) => (
   <div
     data-testid="test-component"
     className={`test-component test-component-${theme}`}
   >
-    <h1 className="heading">I'm the test component</h1>
+    <h1 className="heading">I'm the test componenta</h1>
     <h2>Made with love by Harvey</h2>
   </div>
 );
 
-export default TestComponent;
+export default Header;
