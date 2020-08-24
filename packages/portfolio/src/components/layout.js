485f89412bdb97f184c6ad6e4d00355a3b1a013e
dlarrota <dlarrota@hugeinc.com>
2020-08-24 17:43:33 -0500

    build/ assets with built portfolio

--- packages/portfolio/src/components/layout.js
+++ packages/portfolio/src/components/layout.js
@@ -1,14 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import { Header, Hero } from "@elmono/ui";
+
+import "@elmono/ui/dist/index.css";
 import "./global.css";
 
 const Layout = ({ children }) => {
   return (
     <>
-      <header>title</header>
+      <Header
+        text="Daniel Larrotta"
+        imgUrl="https://larrotta69.github.io/app/img/tigre_50.png"
+      />
+      <Hero text="Heroo" />
       <main>{children}</main>
-      <footer>footer</footer>
     </>
   );
 };
--- packages/portfolio/src/pages/index.js
+++ packages/portfolio/src/pages/index.js
@@ -1,17 +1,7 @@
 import React from "react";
 
 import Layout from "../components/layout";
-import Image from "../components/image";
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-  </Layout>
-);
+const IndexPage = () => <Layout>Under construction</Layout>;
 
 export default IndexPage;
